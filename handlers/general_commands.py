f82694031f036001fc143e241351ce85f1cd179b
PearlFoe <57188977+PearlFoe@users.noreply.github.com>
2020-08-07 15:25:09 +0300

    Update general_commands.py

--- handlers/general_commands.py
+++ handlers/general_commands.py
@@ -4,15 +4,15 @@ from misc import dp, bot
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
 	message_text = ('Привет!\n' +
-					'Это бот-помошник для автопостинга товаров в канал.' +
+					'Это бот-помошник для автопостинга товаров в канал.\n' +
 					'Напиши /help, чтобы получить список возможных команд.')
 	await bot.send_message(message.from_user.id, message_text)
 
 @dp.message_handler(commands=['help'])
 async def send_commands_list(message: types.Message):
 	message_text = ('/start - начать общение с ботом' +
-					'/help - вывод списка команд' +
-					'/create_new_post - отправить данные о товаре в канал' +
+					'/help - вывод списка команд\n' +
+					'/create_new_post - отправить данные о товаре в канал\n' +
 					'/exit - завершить общение с ботом')
 	await bot.send_message(message.from_user.id,message_text)
 
